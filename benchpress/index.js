c473769f5c4dee540b06480e44a0c94ccd18defd
Leonardo Zizzamia <leonardo.zizzamia@gmail.com>
2015-02-08 20:40:02 -0800

    Working on bechmark folder

--- benchpress/index.js
+++ benchpress/index.js
@@ -9,5 +9,12 @@ var Generator = module.exports = function Generator() {
 util.inherits(Generator, ScriptBase);
 
 Generator.prototype.createBenchmarkFiles = function createBenchmarkFiles() {
-  console.log('cool!')
+  if (['dist'].indexOf(this.name) < 0) {
+  	return false;
+  }
+  var srcPath = 'benchmarks/' + this.name;
+  var distPath = 'benchmarks/' + this.appname;
+  this.template(srcPath + '/benchmark.js', distPath + '/benchmark.js');
+  this.template(srcPath + '/bp.conf.js', distPath + '/bp.conf.js');
+  this.template(srcPath + '/main.html', distPath + '/main.html');
 };
